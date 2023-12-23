df01ac00b80108d5a8c0f44a77beac265ceff4d8
umeshlodha1 <lodhaumesh123@gmail.com>
2023-12-23 18:17:49 +0530

    commit

---
+++ admin/config.php
@@ -0,0 +1,6 @@
+<?php
+$hostname = "http://localhost/web tech project/news-site";
+
+$conn = mysqli_connect("127.0.0.1:3307","root","","news-site") or die("Connection failed : " . mysqli_connect_error());
+
+?>
